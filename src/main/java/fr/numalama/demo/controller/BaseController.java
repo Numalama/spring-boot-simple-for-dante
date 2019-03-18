e8c1c08a71839bbfa07badeb5b6dec1a43f5bdfe
mgonzalez <mgonzalez@feeder.fr>
2019-03-18 21:58:07 +0100

    Controller->Service example with get and post and example data using lombok annotations

--- src/main/java/fr/numalama/demo/controller/BaseController.java
+++ src/main/java/fr/numalama/demo/controller/BaseController.java
@@ -1,18 +1,25 @@
 package fr.numalama.demo.controller;
 
 
+import fr.numalama.demo.model.Example;
 import fr.numalama.demo.service.BaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * WHEN YOU CREATED A CONTROLLER WITH SPRING, YOU NEED TO DEFINED THE CLASS WITH
+ * @RestController
+ */
 @RestController
-@RequestMapping("base")
+
+/**
+ * @RequestMapping
+ * ALL the controller into this class, is mapped to /base
+ */
+@RequestMapping("base/")
 public class BaseController {
 
 
@@ -23,19 +30,61 @@ public class BaseController {
      */
     @Autowired
     private BaseService baseService;
+
     /**
      * LET'S INTRODUCE, THE SERVICES NO ? ( THE WORK SIDE )
      */
 
     private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    /**
+     * WHEN USING A GET MAPPING, DATAS ARE PUSH THROUGHT URL (so we use @PathVariable)
+     * example url: localhost:8080/base/get/{id}
+     */
+    @GetMapping(value = "get/{id}", produces = "application/json")
     public String getTest(@PathVariable int id){
+        // System.out.println("id : " + id);
+        logger.info("id: " + id);
+        return this.baseService.generateIDExample(id);
+    }
 
+    /**
+     * WHEN USING A POST MAPPING, DATAS ARE PUSH THROUGHT BODY OF THE REQUEST (so we use @RequestBody)
+     * example url: localhost:8080/base/post/
+     *
+     * JSONify the object into a json
+     * so Example => Example json object
+     */
+    @PostMapping(value = "post/", produces = "application/json")
+    public void getTestPost(@RequestBody Example example){
+        // System.out.println("id : " + id);
+//        /logger.info("id: " + example.getId());
+
+        //return this.baseService.generateIDExample(id);
+    }
+
+    /**
+     * others example
+     *
+     * @PutMapping
+     * @DeleteMapping
+     * ...
+     */
+
+    /**
+     * @RequestMapping ACCEPT ALL (GET/POST/PUT/DELETE...)
+     *
+     * MORE SIMPLE WHEN YOU START
+     */
+    @RequestMapping(value = "/", produces = "application/json")
+    public String getTestall(@RequestBody int id){
         // System.out.println("id : " + id);
         logger.info("id: " + id);
 
         return this.baseService.generateIDExample(id);
     }
+
+
+
 
 }
---
+++ src/main/java/fr/numalama/demo/model/Example.java
@@ -0,0 +1,23 @@
+package fr.numalama.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data // GETTER AND SETTER
+@AllArgsConstructor // CONSTRUCTOR WITH ALL PARAMETERS
+@NoArgsConstructor // CONSTRUCTO W/OUT PARAMERET
+@ToString // A TOSTRING METHOD
+// easy
+public class Example {
+
+    private Integer id;
+
+    /**
+     * GETTER AND SETTER
+     */
+
+
+}
